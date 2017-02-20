5a3954a71676248fa9be36334464cea2d79ad5ef
Stephen Whitmore <sww@eight45.net>
2017-02-19 22:11:56 -0800

    basic command

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var comandante = require('comandante')
+var spawn = require('child_process').spawn
+var createServer = require('../server')
+
+if (process.argv.length === 2) {
+  console.log('TODO: usage notes')
+  return
+}
+
+switch (process.argv[2]) {
+  case 'install':
+  case 'i':
+  case 'publish':
+    var args = ['--registry', 'http://localhost:9000']
+    args = args.concat(process.argv.slice(2))
+    spawn('npm', args, {stdio:'inherit'})
+    break
+  case 'daemon':
+    createServer(function (err, server) {
+      console.log('listening on http://0.0.0.0:9000')
+    })
+    break
+}
+
